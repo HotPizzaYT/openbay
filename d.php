7aa24f84ff576e8bc2fc0e446c6c1cc2d6075c20
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2023-12-20 21:04:28 -0600

    Fix description box

--- d.php
+++ d.php
@@ -114,7 +114,7 @@
             <br />
             <a href="magnet:?xt=urn:btih:<?php echo $infohash; ?>&dn=<?php echo urlencode($name); ?>&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce">Download this torrent</a>
             <p>
-                <textarea readonly><?php echo $desc; ?></textarea>
+                <textarea readonly style="width: 100%; height: 100vh;"><?php echo $desc; ?></textarea>
 
 
             </p>
